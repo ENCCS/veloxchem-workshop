d7be84dcc71d1635658b342da79bcd67cbd92d38
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-03-01 12:12:40 +0100

    Use sphinx-book-theme

--- content/conf.py
+++ content/conf.py
@@ -3,9 +3,7 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Path setup --------------------------------------------------------------
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -13,20 +11,15 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
-
 project = 'VeloxChem'
 copyright = '2021, The contributors'
 author = 'The contributors'
 github_user = 'ENCCS'
 github_repo_name = 'veloxchem-workshop'  # auto-detected from dirname if blank
 github_version = 'master'
-conf_py_path = '/content/' # with leading and trailing slash
-
+conf_py_path = '/content/'  # with leading and trailing slash
 # -- General configuration ---------------------------------------------------
-
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -36,63 +29,68 @@ extensions = [
     'sphinx_lesson',
     # remove once sphinx_rtd_theme updated for contrast and accessibility:
     'sphinx_rtd_theme_ext_color_contrast',
-    #'sphinx.ext.intersphinx',
+    # 'sphinx.ext.intersphinx',
     "sphinxcontrib.bibtex",
     "sphinx.ext.todo",
     "sphinx_thebe",
 ]
-
 # configure sphinxcontrib.bibtex
 bibtex_bibfiles = ["bibliography.bib"]
-
 # Settings for myst_nb:
 # https://myst-nb.readthedocs.io/en/latest/use/execute.html#triggering-notebook-execution
 #jupyter_execute_notebooks = "off"
 #jupyter_execute_notebooks = "auto"   # *only* execute if at least one output is missing.
 #jupyter_execute_notebooks = "force"
 jupyter_execute_notebooks = "cache"
-
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['README*', '_build', 'Thumbs.db', '.DS_Store',
-                    'jupyter_execute', '*venv*']
-
-
+exclude_patterns = [
+    'README*', '_build', 'Thumbs.db', '.DS_Store', 'jupyter_execute', '*venv*'
+]
 # -- Options for HTML output -------------------------------------------------
-
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 html_logo = "img/VeloxChem.png"
 html_favicon = "img/favicon.ico"
 html_title = project
-
+# sphinx-book-theme options
+html_theme_options = {
+    "repository_url": f"https://github.com/{github_user}/{github_repo_name}",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "repository_branch": "master",
+    "path_to_docs": "content",
+    "use_download_button": True,
+    "home_page_in_toc": True,
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+        "thebe": True,
+        "notebook_interface": "jupyterlab",
+    },
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
-
-thebe_config = {
-    "repository_url": f"https://github.com/{github_user}/{github_repo_name}",
-    "repository_branch": "master",
-}
-
+html_static_path = ['_static']
+html_css_files = ["overrides.css"]
 # HTML context:
-from os.path import dirname, realpath, basename
-html_context = {'display_github': True,
-                'github_user': github_user,
-                # Auto-detect directory name.  This can break, but
-                # useful as a default.
-                'github_repo': github_repo_name or basename(dirname(realpath(__file__))),
-                'github_version': github_version,
-                'conf_py_path': conf_py_path,
-               }
+from os.path import basename, dirname, realpath
 
+html_context = {
+    'display_github': True,
+    'github_user': github_user,
+    # Auto-detect directory name.  This can break, but
+    # useful as a default.
+    'github_repo': github_repo_name or basename(dirname(realpath(__file__))),
+    'github_version': github_version,
+    'conf_py_path': conf_py_path,
+}
 # Intersphinx mapping.  For example, with this you can use
 # :py:mod:`multiprocessing` to link straight to the Python docs of that module.
 # List all available references:
@@ -101,3 +99,31 @@ html_context = {'display_github': True,
 #    #'python': ('https://docs.python.org/3', None),
 #    #'sphinx': ('https://www.sphinx-doc.org/', None),
 #    }
+# Our own customisation
+from sphinx_lesson.directives import _BaseCRDirective
+
+
+class SignatureDirective(_BaseCRDirective):
+    extra_classes = ["toggle-shown", "dropdown"]
+
+
+class ParametersDirective(_BaseCRDirective):
+    extra_classes = ["dropdown"]
+
+
+class TypealongDirective(_BaseCRDirective):
+    extra_classes = ["toggle-shown", "dropdown"]
+
+
+DIRECTIVES = [SignatureDirective, ParametersDirective, TypealongDirective]
+# the epilog
+rst_epilog = f"""
+.. role:: red
+.. role:: blue
+.. _VeloxChem: https://veloxchem.org
+"""
+
+
+def setup(app):
+    for obj in DIRECTIVES:
+        app.add_directive(obj.cssname(), obj)
--- content/index.rst
+++ content/index.rst
@@ -15,7 +15,8 @@ Intro
    :maxdepth: 1
    :caption: The lesson
    
-   intro
+   notebooks/intro
+   visualization
 
 
 
@@ -23,7 +24,8 @@ Intro
    :widths: auto
    :delim: ;
 
-   20 min ; :doc:`intro`
+   20 min ; :doc:`notebooks/intro`
+   20 min ; :doc:`notebooks/visualization`
    20 min ; :doc:`visualization`
 
 
@@ -46,6 +48,51 @@ Who is the course for?
 
 About the course
 ----------------
+
+
+Graphical and text conventions
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+We adopt a few conventions which help organize the material.
+
+Function signatures
+   These are shown in a text block marked with a wrench emoji:
+
+   .. signature:: cmake_minimum_required
+
+      .. code-block:: cmake
+
+         cmake_minimum_required(VERSION <min>[...<max>] [FATAL_ERROR])
+
+   The signature can be hidden by clicking the toggle.
+
+Command parameters
+   The description of the command parameters will appear in a separate text
+   box. It will be marked with a laptop emoji:
+
+   .. parameters::
+
+      ``VERSION``
+          Minimum and, optionally, maximum version of CMake to use.
+      ``FATAL_ERROR``
+          Raise a fatal error if the version constraint is not satisfied. This
+          option is ignored by CMake >=2.6
+
+   The description is hidden and will be shown by clicking the toggle.
+
+Type-along
+   The text and code for these activities are in a separate text box, marked with
+   a keyboard emoji:
+
+   .. typealong:: Let's look at an example
+
+      .. code-block:: cmake
+
+         cmake_minimum_required(VERSION 3.16)
+
+         project(Hello LANGUAGES CXX)
+
+   The content can be hidden by clicking the toggle.
 
 
 
