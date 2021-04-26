06dcafd10636c045f1e480a93fa90c42ae3719e2
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-04-26 17:55:31 +0200

    Update environment.yml

--- content/conf.py
+++ content/conf.py
@@ -3,7 +3,9 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+
 # -- Path setup --------------------------------------------------------------
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -11,6 +13,8 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+
+
 # -- Project information -----------------------------------------------------
 project = 'VeloxChem: quantum chemistry from laptop to HPC'
 copyright = '2021, The contributors'
@@ -20,6 +24,7 @@ github_repo_name = 'veloxchem-workshop'  # auto-detected from dirname if blank
 github_version = 'master'
 conf_py_path = '/content/'  # with leading and trailing slash
 # -- General configuration ---------------------------------------------------
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
@@ -44,15 +49,19 @@ bibtex_bibfiles = ["bibliography.bib"]
 #jupyter_execute_notebooks = "auto"   # *only* execute if at least one output is missing.
 #jupyter_execute_notebooks = "force"
 jupyter_execute_notebooks = "cache"
+
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
     'README*', '_build', 'Thumbs.db', '.DS_Store', 'jupyter_execute', '*venv*'
 ]
+
 # -- Options for HTML output -------------------------------------------------
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
