6c888fa29bed45868d49faf652f5450b333fbd55
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-05-03 13:28:01 +0200

    Further clean up

--- content/conf.py
+++ content/conf.py
@@ -90,7 +90,7 @@ html_sidebars = {"**": ["sbt-sidebar-nav.html", "sbt-sidebar-footer.html"]}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 #html_css_files = ["overrides.css"]
 # HTML context:
 from os.path import basename, dirname, realpath
